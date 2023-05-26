8dcc47cf2ca12346ee211e4560dece872ea1dff9
Hoblovski <daizy19@mails.tsinghua.edu.cn>
2023-05-26 14:41:10 +0800

    stat-maturin

---
+++ stat-rv.py
@@ -0,0 +1,177 @@
+from pprint import pprint
+from typing import Dict, List, Tuple, Optional
+
+# Used for global error report
+ParseLocation = 0
+
+# Constants
+INDIRECT = '<indirect>'
+
+# Configs
+INFILE = 'data/maturin.S'
+DUMP_CALLS = 'calls.txt'
+DUMP_FRAMESZ = 'framesz.txt'
+REMOVE_BACKEDGE = True
+REPORT_CYCLE = True
+
+
+def parse_asm(
+    infile: str
+) -> Tuple[List[str], Dict[str, Optional[int]], Dict[str, List[str]]]:
+    with open(infile, 'r') as fin:
+        lines = fin.readlines()
+
+    curfn = None
+    fns = []
+    framesz = {}
+    calls = {}
+
+    print('parsing..')
+    for lineno, line in enumerate(lines):
+        global ParseLocation
+        ParseLocation = lineno
+        line = line.strip()
+        parts = line.split()
+
+        if len(parts) >= 1 and parts[-1].endswith('>:'):
+            # start of fn
+            assert len(parts) == 2
+            assert parts[1].startswith('<')
+            f = parts[1][1:-2]
+            if 'LBB' not in f:
+                curfn = f
+                fns.append(curfn)
+                framesz[curfn] = None
+                calls[curfn] = []
+
+        elif len(parts) >= 2 and parts[-1].startswith('sp,sp,') and parts[-2] == 'addi':
+            fsz = parts[-1].split(',')[-1]
+            fsz = -int(fsz)
+            if fsz < 0:
+                # de-alloc stack
+                continue
+            if framesz[curfn] is not None:
+                # Assume: only the first is framealloc
+                pass
+            framesz[curfn] = fsz
+
+        elif len(parts) >= 2 and parts[-2] in {'jalr'}:
+            assert len(parts[-1]) == 2  # jalr a0 etc
+            calls[curfn].append(INDIRECT)
+
+        elif len(parts) >= 5 and parts[-5] in {'jalr'}:
+            assert parts[-1].startswith('<')
+            assert parts[-1].endswith('>')
+            callee = parts[-1][1:-1]
+            assert 'LBB' not in callee
+            calls[curfn].append(callee)
+
+        else:
+            if any(part == 'jalr' for part in parts):
+                print(lineno + 1)
+                assert False
+
+    # normalize calls: dedup
+    all_fns = set(fns + [INDIRECT])
+    for caller, callees in calls.items():
+        assert caller in fns
+        callees = sorted(list(set(callees)))
+        if not (set(callees) <= all_fns):
+            print('Unknown functions:')
+            print(set(callees) - all_fns)
+            assert False
+        calls[caller] = callees
+    fns = sorted(fns)
+    print('parsing..done')
+    return fns, framesz, calls
+
+
+def analyze(
+    fns: List[str], framesz: Dict[str, Optional[int]], calls: Dict[str, List[str]]
+):
+    indegrees = {fn: 0 for fn in fns}
+    for caller, callees in calls.items():
+        for callee in callees:
+            if callee != INDIRECT:
+                indegrees[callee] += 1
+    entry_fns = [fn for fn in indegrees if indegrees[fn] == 0]
+
+    # DETECT LOOPS
+    n_backedge_removed = 0
+
+    def dfs(u, curpath, ignore):
+        if u in ignore:
+            return
+        if REMOVE_BACKEDGE:
+            rmidx = []
+        for i, v in enumerate(calls[u]):
+            if v == INDIRECT:
+                continue
+            if v in curpath:
+                if REMOVE_BACKEDGE:
+                    rmidx.append(i)
+                if REPORT_CYCLE:
+                    print('cycle')
+                    print(curpath[curpath.index(v) :] + [v])
+                continue
+            curpath.append(v)
+            dfs(v, curpath, ignore)
+            curpath.pop()
+        if REMOVE_BACKEDGE:
+            nonlocal n_backedge_removed
+            for i in reversed(rmidx):
+                del calls[u][i]
+                n_backedge_removed += 1
+
+    fatfs_ignores = [
+        '_ZN5fatfs3dir24Dir$LT$IO$C$TP$C$OCC$GT$8open_dir17h50e37c39712934e5E',
+        '_ZN5fatfs3dir24Dir$LT$IO$C$TP$C$OCC$GT$10create_dir17h623246eb75b5266fE',
+        '_ZN5fatfs3dir24Dir$LT$IO$C$TP$C$OCC$GT$11create_file17h01fc90d2e60397f4E',
+        '_ZN5fatfs3dir24Dir$LT$IO$C$TP$C$OCC$GT$9open_file17h319ee8b88c01e5b4E',
+        '_ZN5alloc4sync12Arc$LT$T$GT$9drop_slow17hda7c2059023c383bE',
+        '_ZN5fatfs3dir24Dir$LT$IO$C$TP$C$OCC$GT$6remove17hbc5971499ea32268E',
+        '_ZN5fatfs3dir24Dir$LT$IO$C$TP$C$OCC$GT$6rename17ha9d25662b38b9404E',
+    ]
+    ignores = [
+        'rust_begin_unwind',
+        'parse_user_app',
+        '_ZN4core3str16slice_error_fail17hc60ea3a9bdad3357E',
+    ]
+    ignores += fatfs_ignores
+    for fn in fns:
+        for ignore in ignores:
+            if ignore in fn:
+                calls[fn] = []
+
+    def detect_cycles():
+        print('detecting cycles..')
+        for entry_fn in entry_fns:
+            dfs(entry_fn, [entry_fn], ignores)
+        print('cycles detected..')
+
+    detect_cycles()
+
+    max_stacksz = {fn: None for fn in fns}
+
+    def get_max_stacksz(u):
+        if max_stacksz[u] is not None:
+            return max_stacksz[u]
+        max_callee = max(
+            (get_max_stacksz(v) for v in calls[u] if v != INDIRECT), default=0
+        )
+        max_stacksz[u] = (framesz[u] or 0) + max_callee
+        return max_stacksz[u]
+
+    for entry_fn in entry_fns:
+        get_max_stacksz(entry_fn)
+    pprint(max_stacksz)
+    print(max(max_stacksz.items(), key=lambda x: x[1] or 0))
+
+
+fns, framesz, calls = parse_asm(INFILE)
+with open(DUMP_CALLS, 'w') as fout:
+    for caller, callees in calls.items():
+        print(caller, ':', ' '.join(callees), file=fout)
+with open(DUMP_FRAMESZ, 'w') as fout:
+    pprint(framesz, stream=fout)
+analyze(fns, framesz, calls)
